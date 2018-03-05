625caeb43a23dfa63e9d2d8207b79ee512b36f8e
wipro_chef_user <35237027+balagsundaram@users.noreply.github.com>
2018-03-05 12:52:22 +0000

    Removing cookbook

--- ad-join-cookbook-master/ad-join-cookbook-master/test/SUPERMARKET.md
+++
@@ -1,3 +0,0 @@
-The command to upload to the chef supermarket
-    
-    CHEF_ENV=spuder knife cookbook site share "ad-join" "Operating Systems & Virtualization" -z --dry-run
--- ad-join-cookbook-master/ad-join-cookbook-master/test/spec/ad-join/3_user_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'spec_helper'
-
-describe user('Administrator') do
-  it { should exist }
-end
-describe user('Administrator') do
-  it { should belong_to_group 'administrators' }
-end
